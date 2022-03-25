20ab42fca650c6971fdb6890dac44a93402c22af
hossainrabbi <hossainrabbi.dev@gmail.com>
2022-03-25 16:50:04 +0600

    start shopping cart project with tailwindcss

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="text-gray-700 text-center text-lg transition-colors hover:text-gray-500">
+      Hello World
+    </div>
+  );
+}
