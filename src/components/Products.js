ea15d0594b939ddf343f256618e8c6fde5d5e615
hossainrabbi <hossainrabbi.dev@gmail.com>
2022-03-26 13:06:10 +0600

    pagination added in home page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { useProductsContext } from '../contexts/ProductsContext';
 import Product from './Product';
 
 export default function Products() {
   const { loading, error, products } = useProductsContext();
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersParPage = 8;
+  const pagesVisited = pageNumber * usersParPage;
+
+  const displayProducts = products.slice(
+    pagesVisited,
+    pagesVisited + usersParPage
+  );
+
+  const pageCount = Math.ceil(products.length / usersParPage);
 
   if (loading) {
     return <div className="main__container">Loading...</div>;
@@ -13,16 +25,28 @@ export default function Products() {
     return <div className="main__container">Server site Error</div>;
   }
 
-  return products.length > 0 ? (
-    <section className="main__container">
+  return displayProducts.length > 0 ? (
+    <section className="main__container mb-36">
       <h2 className="mt-16 mb-8 uppercase text-center text-4xl text-gray-800">
         Products
       </h2>
       <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {products.map((product) => (
+        {displayProducts.map((product) => (
           <Product {...product} key={product.id} />
         ))}
       </div>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={({ selected }) => setPageNumber(selected)}
+        containerClassName={'flex justify-center items-center gap-x-4 py-10'}
+        previousLinkClassName="btn_normal"
+        nextLinkClassName="btn_normal"
+        pageLinkClassName="btn_normal"
+        activeLinkClassName="btn_normal bg-gray-900 text-gray-200"
+        disabledLinkClassName="btn_normal text-gray-500 hover:text-gray-500 hover:bg-gray-200 cursor-not-allowed"
+      />
     </section>
   ) : (
     <div className="main__container">Product not Found</div>
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,7 +3,12 @@ import { FaRegStar, FaStar } from 'react-icons/fa';
 
 export default function Rating({ rate, onClick, ...rest }) {
   return [...Array(5)].map((_, i) => (
-    <span className="text-orange-500" onClick={(i) => onClick(i)} {...rest}>
+    <span
+      className="text-orange-500"
+      onClick={(i) => onClick(i)}
+      {...rest}
+      key={i}
+    >
       {Math.round(rate) > i ? <FaStar /> : <FaRegStar />}
     </span>
   ));
